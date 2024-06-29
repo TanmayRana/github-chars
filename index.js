877c5d2cc1bf60d876b81a0a3768cfa0c777bd26
Tanmay Rana <tanmayrana2001@gmail.com>
2024-06-29 10:00:57 +0530

    push all code

---
+++ index.js
@@ -0,0 +1,16 @@
+const name = "Tanmay Rana";
+console.log(name);
+
+const age = 21;
+const dob = "20/06/2003";
+
+const sayName = () => {
+  console.log("My name is " + name);
+  console.log("I am " + age + " years old");
+};
+
+sayName();
+
+const sum = (a, b) => {
+  return a + b;
+};
