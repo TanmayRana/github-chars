ab86fcdc7571d03772b56387dcd2bc3cdd02d072
Tanmay Rana <tanmayrana2001@gmail.com>
2024-10-30 19:49:32 +0530

    new feat added  in branch2

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ function Hello(data) {
 }
 
 Hello({ naem: "Tanmay", email: "tanmay@gmail.com" });
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const newArr = arr.map((item) => {
+  return item * 2;
+});
+console.log(newArr);
